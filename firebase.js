6eaea0654510a5edf679c2da5e606320ca58a86b
Muhammed-Hagras <40708938+Muhammed-Hagras@users.noreply.github.com>
2023-04-15 05:17:31 +0200

    initailize firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFMeE3YY1fKa4b3BpD810UfHqXs1GAZ7c",
+  authDomain: "clone-44dd0.firebaseapp.com",
+  projectId: "clone-44dd0",
+  storageBucket: "clone-44dd0.appspot.com",
+  messagingSenderId: "1065768561985",
+  appId: "1:1065768561985:web:79b31b16c6b97bbef205c0",
+  measurementId: "G-355CYVCLH0",
+};
